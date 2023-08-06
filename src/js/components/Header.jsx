b526325562701cfa80e5f96dfa010e4bef68b1e9
Erik Suris <erikvsuris@gmail.com>
2023-08-06 16:08:46 -0300

    Polishing footer and components

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,15 @@
+export default () => {    
+    return (
+      <header>
+        <a href="#home">
+            <img class="icon" src="src/assets/svg/icon.svg" alt="Ícone da Empório Pet"/>
+        </a>
+        <nav>
+            <a class="current-page" href="#home">Home</a>
+            <a href="#sobre">Sobre</a>
+            <a href="#servicos">Serviços</a>
+            <a href="#contato">Contato</a>
+        </nav>
+      </header>
+    );
+};
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './components/Header'
 import Carousel from './components/Carousel'
 import Copyright from './components/Copyright'
 
+const headerRoot = ReactDOM.createRoot(document.getElementById('react-header'))
 const carouselRoot = ReactDOM.createRoot(document.getElementById('react-carousel'))
 const copyrightRoot = ReactDOM.createRoot(document.getElementById('react-copyright'))
+
+headerRoot.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+)
 
 carouselRoot.render(
   <React.StrictMode>
