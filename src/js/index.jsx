0ec54813293134da0489f201c333d008f99b6fdc
Erik Suris <erikvsuris@gmail.com>
2023-08-10 16:00:16 -0300

    Polishing grid

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Header from './components/Header'
 import Carousel from './components/Carousel'
 import Copyright from './components/Copyright'
 
-const headerRoot = ReactDOM.createRoot(document.getElementById('react-header'))
 const carouselRoot = ReactDOM.createRoot(document.getElementById('react-carousel'))
 const copyrightRoot = ReactDOM.createRoot(document.getElementById('react-copyright'))
-
-headerRoot.render(
-  <React.StrictMode>
-    <Header />
-  </React.StrictMode>,
-)
 
 carouselRoot.render(
   <React.StrictMode>
