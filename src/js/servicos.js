0c05cf3a7d4888eb2bde924107a5ae1b6d9f2b0c
Erik Suris <erikvsuris@gmail.com>
2023-08-17 13:06:45 -0300

    Mobile First

---
+++ src/js/servicos.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        titulo: 'Banho e Tosa',
+        imagem: 'url',
+
+    },
+    {
+        titulo: 'Vacinas',
+        imagem: 'url',
+
+    },
+    {
+        titulo: 'Pet Shop',
+        imagem: 'url',
+
+    }
+]
